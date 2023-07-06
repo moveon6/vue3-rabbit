979a7925a03fda2d26a5164ed4e4db30fa943651
moveon6 <2354481356@qq.com>
2023-07-06 12:39:20 +0800

    新鲜好物和人气推荐实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -6,3 +6,20 @@ export function getBannerAPI() {
     url: '/home/banner'
   })
 }
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+  return httpInstance({
+    url: '/home/new'
+  })
+}
+
+export const findHotAPI = () => {
+  return httpInstance({
+    url: '/home/hot'
+  })
+}
