045066d5fd08bb62d129918fb5f775bc457140a6
moveon6 <2354481356@qq.com>
2023-07-08 00:32:34 +0800

    列表无限加载功能实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -35,6 +35,20 @@ const categoryData = ref({})
     console.log('tab切换了',reqData.value.sortField);
     reqData.value.page=getGoodList()
   }
+
+  // 加载更多
+  const disabled=ref(false)
+  const load=async()=>{
+    console.log('加载更多数据')
+    // 获取下一页的数据
+    reqData.value.page++
+    const res = await getSubCategoryAPI(reqData.value)
+    goodList.value=[...goodList.value,...res.result.items]
+    // 加载完毕 停止监听
+    if(res.result.items.length===0){
+      disabled.value=true
+    }
+  }
 </script>
 
 <template>
@@ -54,7 +68,7 @@ const categoryData = ref({})
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load">
          <!-- 商品列表-->
          <GoodsItem v-for="goods in goodList" :key="goods.id" :goods="goods"></GoodsItem>
       </div>
