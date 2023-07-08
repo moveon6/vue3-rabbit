2f0006c47eec757ab6902364a8f9d7caf0356619
moveon6 <2354481356@qq.com>
2023-07-08 12:48:06 +0800

    热搜榜-组件封装

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,6 +2,8 @@
 import { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import DetailHot from '@/views/Detail/components/DetailHot.vue'
+
 const goods = ref({})
 const route = useRoute()
 const getGoods = async () => {
@@ -120,7 +122,10 @@ onMounted(() => getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-
+              <!-- 24小时 -->
+              <DetailHot></DetailHot>
+              <!-- 周 -->
+              <DetailHot></DetailHot>
             </div>
           </div>
         </div>
